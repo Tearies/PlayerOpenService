684fa0eb2c437af0079bc1e77ead507e884595c1
Tearies <maojun_huier@sina.com>
2018-12-24 17:19:57 +0800

    修复页面错误

--- Middleware/ApiIndexPage/ApiIndexPageView.cs
+++ Middleware/ApiIndexPage/ApiIndexPageView.cs
@@ -17,8 +17,8 @@ namespace WebApi
             this.WriteLiteral("</title>\r\n");
             this.WriteLiteral("<style>*{user-select: none; -webkit-user-select: none; -khtml-user-select: none; -moz-user-select: none; -ms-user-select: none; cursor: default;}  #vertext{text-align:right;} #verheader{font-size:20px; color:#f00; } #headertext{text-align:center;} #header{font-size:24px; } #api{font-size:16px;color: #111; cursor: pointer;} #api:hover{color: #0088ff;} #api:active{color: #0088ff;}</style>");
             this.WriteLiteral("<body>");
-            this.WriteLiteral($"<div id='header'><H1 id='headertext'>{ApiFactory.ServiceName}</H1><div>");
-            this.WriteLiteral($"<div id='verheader'><H4 id='vertext'>version:{ApiFactory.ApiVer}</H4><div>");
+            this.WriteLiteral($"<div id='header'><H1 id='headertext'>{ApiFactory.ServiceName}</H1></div>");
+            this.WriteLiteral($"<div id='verheader'><H4 id='vertext'>version:{ApiFactory.ApiVer}</H4></div>");
             int offset = 1;
             foreach (var api in ApiFactory.Factory.ApiIndexList)
             {
